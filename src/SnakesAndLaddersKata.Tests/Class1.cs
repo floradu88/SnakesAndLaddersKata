ef8ca617a28dc8fa83f55553246a8fcccfed5982
Radu Florescu <radu.florescu@hotmail.com>
2018-10-18 10:35:13 +0300

    Initial commit with basic project structure.

---
+++ src/SnakesAndLaddersKata.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SnakesAndLaddersKata.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void should_test()
+        {
+        }
+    }
+}
